0637846a64e8abc2a243868494cc5f868d44717a
yvijay <yvijay@MAA-5CG7240WW0.ant.amazon.com>
2020-07-20 01:55:16 +0530

    First commit

---
+++ LearnJave/src/javaProject/HelloWorld.java
@@ -0,0 +1,12 @@
+package javaProject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world....");
+		System.out.println("My first program....");
+	}
+
+}
